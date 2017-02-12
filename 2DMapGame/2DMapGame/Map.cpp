dc898c71dabf0d775b9e72591136031387712e27
Pedisman <none@none>
2017-02-13 02:29:04 +1300

    added template constructor to generate wall vector for map

---
+++ 2DMapGame/2DMapGame/Map.cpp
@@ -0,0 +1,26 @@
+#include "Map.h"
+
+WallMap myWallMap(map);
+
+template <typename T, int Tx, int Ty>
+WallMap::WallMap(T (&inputArray) [Tx][Ty])
+{
+	for (int i = 0; i < Tx; ++i)
+	{
+		for (int j = 0; j < Ty; ++j)
+		{
+			if (map[i][j] == WALL_BLOCK)
+			{
+				blocks.push_back(WallBlock(i, j));
+			}
+		}
+	}
+}
+
+void WallMap::drawMap() const
+{
+	for (auto itr = blocks.begin(); itr != blocks.end(); ++itr)
+	{
+		itr->drawBlock();
+	}
+}
