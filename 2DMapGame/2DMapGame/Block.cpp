2f048c7ed42a26d1a0d9939e13d793c9cd82801d
Pedisman <none@none>
2017-02-13 02:35:31 +1300

    Implemented wall block color definition in wall block draw method

--- 2DMapGame/2DMapGame/Block.cpp
+++ 2DMapGame/2DMapGame/Block.cpp
@@ -10,6 +10,7 @@ Block::Block(int x, int y)
 void WallBlock::drawBlock() const
 {
 	glBegin(GL_QUADS);
+	glColor3d(wallBlockColor[0], wallBlockColor[1], wallBlockColor[2]);
 	glVertex2d(x*width, y*height);
 	glVertex2d(x*width + width, y*height);
 	glVertex2d(x*width + width, y*height + height);
