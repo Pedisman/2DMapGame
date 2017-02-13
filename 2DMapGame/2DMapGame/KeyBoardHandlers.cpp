3c6a76d520e6dd13bb6fbdca6eacdf7c92282169
Pedisman <none@none>
2017-02-13 14:04:47 +1300

    defined keyhandlers for key press and release events

--- 2DMapGame/2DMapGame/KeyBoardHandlers.cpp
+++ 2DMapGame/2DMapGame/KeyBoardHandlers.cpp
@@ -4,8 +4,48 @@
 
 void normalKeyHandler(unsigned char key, int x, int y)
 {
-	if (key == ESCAPE)
+	if (key == ESCAPE_KEY)
 	{
 		exit(0);
+	}
+}
+
+void keyPressHandler(int key, int x, int y)
+{
+	if (key == LEFT_KEY)
+	{
+		pressedKeys[LEFT] = true;
+	}
+	else if (key == UP_KEY)
+	{
+		pressedKeys[UP] = true;
+	}
+	else if (key == RIGHT_KEY)
+	{
+		pressedKeys[RIGHT] = true;
+	}
+	else if (key == DOWN_KEY)
+	{
+		pressedKeys[DOWN] = true;
+	}
+}
+
+void keyReleaseHandler(int key, int x, int y)
+{
+	if (key == LEFT_KEY)
+	{
+		pressedKeys[LEFT] = false;
+	}
+	else if (key == UP_KEY)
+	{
+		pressedKeys[UP] = false;
+	}
+	else if (key == RIGHT_KEY)
+	{
+		pressedKeys[RIGHT] = false;
+	}
+	else if (key == DOWN_KEY)
+	{
+		pressedKeys[DOWN] = false;
 	}
 }
--- 2DMapGame/2DMapGame/KeyBoardHandlers.h
+++ 2DMapGame/2DMapGame/KeyBoardHandlers.h
@@ -2,5 +2,7 @@
 #define KEYBOARD_HANDLERS_h
 
 void normalKeyHandler(unsigned char key, int x, int y);
+void keyPressHandler(int key, int x, int y);
+void keyReleaseHandler(int key, int x, int y);
 
 #endif // KEYBOARD_HANDLERS_h
